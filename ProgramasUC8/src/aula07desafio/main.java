68384fdc18c2f5b68c9a3be0c9a6ac33db3eb077
eeRaike <143718700+eeRaike@users.noreply.github.com>
2024-07-09 02:37:29 -0300

    aaaa

---
+++ ProgramasUC8/src/aula07desafio/main.java
@@ -0,0 +1,16 @@
+
+package aula07desafio;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+        RadioBolso radio = new RadioBolso();
+        
+        radio.onOffRadio();
+        radio.play();
+        radio.volumeAumenta();
+        
+    }
+    
+}
